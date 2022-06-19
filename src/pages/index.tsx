a56406e5af58dbe453510d58c00526cb1fc3c746
Noel Mace <contact@noelmace.com>
2022-06-19 02:27:18 +0200

    feat: add eplainer sections to home page
    close #7

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,42 +5,151 @@ import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import styles from './index.module.css';
 import GitHubButton from 'react-github-btn';
+import OpenSourceSVG from './illustrations/open-source.svg';
+import FilesSentSVG from './illustrations/files-sent.svg';
+import ScienceSVG from './illustrations/alien-science.svg';
 
-export default function Home(): JSX.Element {
+function Banner() {
+  return (
+    <div>
+      <div className={styles.banner}>
+        Want to learn more about the blindnet company?
+        <br />
+        Go check <a href="https://blindnet.io">blindnet.io</a>&#8202;.
+      </div>
+    </div>
+  );
+}
+
+function HeaderHero() {
   const { siteConfig } = useDocusaurusContext();
   return (
-    <Layout title="" description="">
-      <div>
-        <div className={styles.banner}>
-          Want to learn more about the blindnet company?<br />
-            Go check <a href="https://blindnet.io">blindnet.io</a>
+    <header className={clsx('hero', styles.heroBanner)} data-theme="dark">
+      <div className={clsx('container', styles.heroBanner__container)}>
+        <h1 className={clsx('hero__title', styles.heroBanner__title)}>blindnet&#8203;<b>.dev</b></h1>
+        <p className="hero__subtitle">{siteConfig.tagline}</p>
+        <div className={styles.indexCtas}>
+          <Link className="button button--primary" to="https://join.slack.com/t/blindnet/shared_invite/zt-1arqlhqt3-A8dPYXLbrnqz1ZKsz6ItOg">
+            Join Slack
+          </Link>
+          <Link className="button button--secondary" to="/docs/quickstart">
+            Start Encrypting!
+          </Link>
+          <span className={styles.indexCtasGitHubButtonWrapper}>
+            <GitHubButton
+              className={styles.indexCtasGitHubButton}
+              href="https://github.com/blindnet-io/blindnet.dev"
+              data-size="large"
+              aria-label="Star blindnet-io/blindnet.dev on GitHub"
+            >
+              Star
+            </GitHubButton>
+          </span>
         </div>
       </div>
-      <header className={clsx('hero', styles.heroBanner)}>
-        <div className="container">
-          <h1 className="hero__title">{siteConfig.title}</h1>
-          <p className="hero__subtitle">{siteConfig.tagline}</p>
+    </header>
+  );
+}
+
+function E2EEncryption() {
+  return (
+    <section className={styles.explainerSection}>
+      <div className={styles.twoColumns}>
+        <div className={styles.twoColumns__column}>
+          <FilesSentSVG className={styles.twoColumns__illustration} height="400px" />
+        </div>
+        <div className={styles.twoColumns__column}>
+          <h2>E2E Encryption in a Snap</h2>
+          <p>
+            Our JavaScript, PHP and Node.js SDKs, parts of the blindnet DevKit, allows to manage encrypted data with just a few lines of code, without requirering any complexe set-up or expertise in
+            cryptography.
+          </p>
+          <p>
+            Follow our <a href="/docs/quickstart">Quick Start</a> guide to see how in a few minutes, or read the advanced{' '}
+            <a href="/docs/guides">Guides</a> to learn more.
+          </p>
           <div className={styles.indexCtas}>
             <Link className="button button--primary" to="/docs/quickstart">
               Get Started!
             </Link>
-            <Link className="button button--info" to="/docs/guides">
+            <Link className="button button--secondary" to="/docs/guides">
               Follow the Guides
             </Link>
-            <span className={styles.indexCtasGitHubButtonWrapper}>
-              <GitHubButton
-                className={styles.indexCtasGitHubButton}
-                href="https://github.com/blindnet-io/blindnet.dev"
-                data-size="large"
-                aria-label="Star blindnet-io/blindnet.dev on GitHub"
-              >
-                Star
-              </GitHubButton>
-            </span>
           </div>
         </div>
-      </header>
-      <main style={{ marginTop: '10px', textAlign: 'center' }}></main>
+      </div>
+    </section>
+  );
+}
+
+function OpenSource() {
+  return (
+    <section className={clsx(styles.explainerSection, styles.tint)}>
+      <div className={styles.twoColumns}>
+        <div className={styles.twoColumns__column}>
+          <h2>Always Open</h2>
+          <p>At blindnet, we strongly think Openness and Privacy are two sides of the same coin.</p>
+          <p>
+            We meticulously follow the{' '}
+            <a href="https://github.com/blindnet-io/openness-framework/blob/main/principles.md" target="_blank" rel="noopener noreferrer">
+              Openness Principles
+            </a>{' '}
+            in all matters, guided by the{' '}
+            <a href="https://github.com/blindnet-io/openness-framework" target="_blank" rel="noopener noreferrer">
+              Openness Framework
+            </a>{' '}
+            we defined and constantly update.
+          </p>
+          <p>
+            Meaning all components of blindnet DevKit are (or will be soon) open source, but not only. All related contents (including
+            discussions, decisions, documentation and specifications) are and will always stay available to anyone and open to contribution.
+          </p>
+        </div>
+        <div className={styles.twoColumns__column}>
+          <OpenSourceSVG className={styles.twoColumns__illustration} height="400px" />
+        </div>
+      </div>
+    </section>
+  );
+}
+
+function Construction() {
+  return (
+    <section className={styles.explainerSection}>
+      <div className={styles.twoColumns}>
+        <div className={styles.twoColumns__column}>
+          <ScienceSVG className={styles.twoColumns__illustration} height="400px" />
+        </div>
+        <div className={styles.twoColumns__column}>
+          <h2>Constantly Innovating</h2>
+          <p>
+            Our team of PhDs, engineers, DevX experts and developers is constantly working on new ways to improve and extend the blindnet DevKit.
+          </p>
+          <p>
+            We are always open to new ideas and suggestions, especially from <strong>You the developers</strong>, to help us build exactly what <strong>You</strong> need in order to make privacy ubiquitous.
+          </p>
+          <div className={styles.indexCtas}>
+            <Link className="button button--primary" to="https://github.com/blindnet-io">
+              Watch and Contribute
+            </Link>
+            <Link className="button button--secondary" to="https://github.com/blindnet-io/devrel-management/issues/new?assignees=noelmace&labels=request%2Ctriage&template=request.yml&title=%5BRequest%5D%3A+">
+              Make a Wish
+            </Link>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default function Home(): JSX.Element {
+  return (
+    <Layout title="" description="">
+      <Banner />
+      <HeaderHero />
+      <E2EEncryption />
+      <OpenSource />
+      <Construction />
     </Layout>
   );
 }
